1f37f00895bf2bf2327ed7bf6b377d43c5fc10fd
popcodelab <pop.code.lab13@gmail.com>
2024-03-20 13:06:29 +0700

    Fix index error. Previous version was wrong.

--- medals_and_flags_scraper.py
+++ medals_and_flags_scraper.py
@@ -17,51 +17,52 @@ def scrape_medals_and_flags(soup: BeautifulSoup) -> list[tuple[str, str, str, in
         cpt = 1
         result: list[tuple[str, str, str, int, int, int, int]] = []
         for div_line in div_lines:
-            # Find the next sibling element
-            next_sibling = div_line.find_next_sibling()
-            # Print the text of the next sibling element
-            if next_sibling:
-                # Index is only used for logging
-                index = next_sibling.findNext('div').get('data-row-id').replace("country-medal-row-","")
-                print(f"treatment of line number :{index}")
-                flag_img = next_sibling.findNext("img")
-                if flag_img:
-                    country = flag_img.get('alt')
-                    flag_src = flag_img.get('src')
-                    img_name = flag_src.split('/')[-1]
-                    file_extension = get_file_extension_from_content_type(flag_src)
-                    # File extension has been found
-                    if file_extension != "file_extension_not_found":
-                        img_filename = img_name + file_extension
-                        print(f"Downloading : {img_filename}")
-                        img_path = os.path.join(download_dir, img_filename)
-                        # Download the image
-                        try:
-                            urllib.request.urlretrieve(flag_src, img_path)
-                            print(f"Downloaded: {img_filename}")
-                        except IOError as io_ex:
-                            print(f"Could not download: {img_filename} : ", io_ex)
-                            continue
-                    else:
-                        print(f"Cannot retrieve the file extension within content type !")
-                        continue
-                else:
-                    print("Flag image not found !")
-                    continue
-            if next_sibling is None:
+            if div_line is None:
                 print("No next line, end of treatment")
                 break
-            next_sibling = next_sibling.find_next_sibling()
-            if next_sibling:
-                tri_letter_code = next_sibling.findNext('div', attrs={'data-cy': 'tri-letter-code'}).text
+            # Index is only used for logging
+            try:
+                index = div_line.findNext('div').get('data-row-id').replace("country-medal-row-","")
+            except AttributeError as attr_ex:
+                print("End of treatment, there is no more row : ", attr_ex.args)
+                break
+            print(f"treatment of line number :{index}")
+            flag_img = div_line.findNext("img")
+            if flag_img:
+                country = flag_img.get('alt')
+                flag_src = flag_img.get('src')
+                img_name = flag_src.split('/')[-1]
+                file_extension = get_file_extension_from_content_type(flag_src)
+                # File extension has been found
+                if file_extension != "file_extension_not_found":
+                    img_filename = img_name + file_extension
+                    print(f"Downloading : {img_filename}")
+                    img_path = os.path.join(download_dir, img_filename)
+                    # Download the image
+                    try:
+                        urllib.request.urlretrieve(flag_src, img_path)
+                        print(f"Downloaded: {img_filename}")
+                    except IOError as io_ex:
+                        print(f"Could not download: {img_filename} : ", io_ex)
+                        continue
+                else:
+                    print(f"Cannot retrieve the file extension within content type !")
+                    continue
+            else:
+                print("Flag image not found !")
+                continue
+
+            div_line = div_line.find_next_sibling()
+            if div_line:
+                tri_letter_code = div_line.findNext('div', attrs={'data-cy': 'tri-letter-code'}).text
             else:
                 print("No tri-letter code found")
                 continue
-            next_sibling = next_sibling.find_next_sibling()
-            if next_sibling:
+            div_line = div_line.find_next_sibling()
+            if div_line:
                 # Gold medals
                 try:
-                    gold_medals_count = next_sibling.findNext('div', attrs={'title': 'Gold'}).text
+                    gold_medals_count = div_line.findNext('div', attrs={'title': 'Gold'}).text
                     if gold_medals_count == '-':
                         gold_medals_count = '0'
                 except AttributeError as attr_ex:
@@ -72,11 +73,11 @@ def scrape_medals_and_flags(soup: BeautifulSoup) -> list[tuple[str, str, str, in
             else:
                 print("No gold medal found")
                 continue
-            next_sibling = next_sibling.find_next_sibling()
-            if next_sibling:
+            div_line = div_line.find_next_sibling()
+            if div_line:
                 try:
                     # Silver medals
-                    silver_medals_count = next_sibling.findNext('div', attrs={'title': 'Silver'}).text
+                    silver_medals_count = div_line.findNext('div', attrs={'title': 'Silver'}).text
                     if silver_medals_count == '-':
                         silver_medals_count = '0'
                 except AttributeError as attr_ex:
@@ -86,11 +87,11 @@ def scrape_medals_and_flags(soup: BeautifulSoup) -> list[tuple[str, str, str, in
             else:
                 print("No silver medal found")
                 continue
-            next_sibling = next_sibling.find_next_sibling()
-            if next_sibling:
+            div_line = div_line.find_next_sibling()
+            if div_line:
                 try:
                     # Bronze medals
-                    bronze_medals_count = next_sibling.findNext('div', attrs={'title': 'Bronze'}).text
+                    bronze_medals_count = div_line.findNext('div', attrs={'title': 'Bronze'}).text
                     if bronze_medals_count == '-':
                         bronze_medals_count = '0'
                 except AttributeError as attr_ex:
